1b4524cb9926d29857610dba0217530cb5e806fb
5335510 <5335510@naver.com>
2022-05-08 22:54:06 +0900

    2022 05-08 22:54

---
+++ src/com/javaex/ex15/MapExam.java
@@ -0,0 +1,21 @@
+
+package com.javaex.ex15;
+
+import java.util.*;
+
+public class MapExam {
+
+	public Map<String, Integer> makeMap(){
+		Map<String, Integer> products = new HashMap<>();
+		
+		products.put("가위", 2500);
+		products.put("크레파스", 5000);
+		
+		return products;
+	}
+
+	
+	
+}
+
+
