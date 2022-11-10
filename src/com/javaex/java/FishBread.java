96557023ac7fdc2106ae73fcbe2db59b4bfe1296
5335510 <5335510@naver.com>
2022-11-10 17:43:27 +0900

    2022 11-10

---
+++ src/com/javaex/java/FishBread.java
@@ -0,0 +1,25 @@
+package com.javaex.java;
+
+//클래스
+public class FishBread {
+	//필드
+	//생성자
+	//메서드gs
+	//메서드일반
+}
+
+class Main {
+	public static void main(String[] args) {
+		//붕어빵, 슈크림 붕어빵의 객체
+		FishBread fishBread;
+		FishBread creamFishBread;
+		
+		//인스턴스화
+		fishBread = new FishBread();
+		creamFishBread = new FishBread();
+	}
+}
+
+
+
+
